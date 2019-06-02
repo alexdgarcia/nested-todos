e9ee4f0701827e6de94a2618548d4d0ba01f825f
Alex Dominguez Garcia <alexand.dominguez@gmail.com>
2019-06-02 10:42:06 -0400

    Revert "Revert "Reworked toggleNotesOff() so that notes are removed appropriately.""
    This reverts commit f3211e6084da9591ed4c61ebb08e43307d1e4f2d.

--- js/app.js
+++ js/app.js
@@ -192,6 +192,9 @@
 				this.toggleNotesOn(e);
 			} else if (e.target.classList[1] === 'show-notes' && e.type === 'focusout') {
 				this.toggleNotesOff(e);
+			} else if (e.target.classList[1] === 'show-notes' && e.target.textContent === '' && e.type === 'keydown' && e.which === 8) {
+				e.preventDefault();
+				this.toggleNotesOff(e);
 			}
 		},
 
@@ -480,8 +483,11 @@
 			var index = this.getTodoIndex(this.todos, parentListItemID);
 			e.target.blur();
 			e.target.classList.remove('show-notes');
-			e.target.classList.add('notes-preview');
 			array[index].notes = e.target.innerHTML;
+
+			if (e.target.textContent !== '') {
+				e.target.classList.add('notes-preview');
+			}
 
 			if (e.type === 'keydown') {
 				this.editTodo(e);
